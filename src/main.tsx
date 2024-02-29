04de72b85a5e8080f3751f99938b758a76d812ef
Troels-dat21B <troe1912@stud.kea.dk>
2024-02-29 12:36:01 +0100

    Init commit

--- src/main.tsx
+++ src/main.tsx
@@ -9,6 +9,7 @@ ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <ChakraProvider>
       <App />
+      
     </ChakraProvider>
   </React.StrictMode>
 )
