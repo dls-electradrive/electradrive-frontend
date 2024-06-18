1f6672503abbfd9e8bc99a354d29813e534baf8d
victor-dat21b <vict702k@stud.kea.dk>
2024-06-18 13:43:37 +0200

    Fixed build error in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,24 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    outDir: 'build', // Specify the output directory
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom'],
+        },
+      },
+      onwarn(warning, warn) {
+        // Ignore "use client" warnings
+        if (warning.code === 'MODULE_LEVEL_DIRECTIVE') {
+          return;
+        }
+        warn(warning);
+      },
+    },
+  },
+});
